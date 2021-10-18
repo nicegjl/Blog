29413706c2deaed607dced36099e22fe747987ef
gongjiali <gongjiali@xindong.com>
2021-10-18 15:37:12 +0800

    fix(): 初始化

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Index from '@/views/index'
+
+Vue.use(Router)
+
+export default new Router({
+	mode: 'history',
+	routes: [
+		{
+			path: '/',
+			name: 'Index',
+			component: Index
+		}
+	]
+})
